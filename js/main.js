5d022a7c22d7e9c9410d8781cd2d79b6ffb7cea3
Zachary Schreiber <zachgives@gmail.com>
2016-10-07 12:53:44 -0400

    stuff

--- js/main.js
+++ js/main.js
@@ -8,4 +8,41 @@ $('#userInput').keyup(function() {
     }).hide();
 });
 
-$('')
+// nav event handlers
+
+$("#action").on("click", function() {
+  movieQuery();
+});
+$("#horror").on("click", function() {
+  movieQuery();
+});
+$("#comedy").on("click", function() {
+  movieQuery();
+});
+$("#fantasy").on("click", function() {
+  movieQuery();
+});
+$("#drama").on("click", function() {
+  movieQuery();
+});
+$("ul li:nth-child(6)").on("click", function() {
+  movieQuery();
+});
+
+//beginings of searchbar function
+
+
+
+function movieQuery() {
+  var searchbar = $("#userInput").val("");
+    $.ajax({
+        url: "api/genre/horror",
+        dataType: "text/json",
+        method: "GET",
+    }).done(function(response) {
+      for (var index = 0; response.data.length; index++) {
+    new targetInfo(response.data[index]);
+   }
+});
+}
+movieQuery();
