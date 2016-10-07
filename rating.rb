d02f932e4542aa245107be3403cdcf4c91a443dc
Brandon Fitch <fitchbj@gmail.com>
2016-10-07 14:26:07 -0400

    Server error resolved.  able to use API's.

--- rating.rb
+++ rating.rb
@@ -1,10 +1,8 @@
 require 'active_record'
+require_relative 'schema'
+require_relative 'environment'
 
 class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
-
-  def initialize
-    
-  end
 end
