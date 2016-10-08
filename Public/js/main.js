040a9b790d2d6ae8b24781546952fec09c709923
Lindsey Adams <linzma87@gmail.com>
2016-10-08 00:01:27 -0400

    Nav/header change content working

--- Public/js/main.js
+++ Public/js/main.js
@@ -56,13 +56,39 @@ movieQuery();
 
 // toggle classes
 
+// click on genre, populate with genre movies
+
 $('.navBar').on('click', '.genre', function(event) {
 
-    $(this).toggleClass('active');
+    var currentTab = $(this).attr('id');
+    $('#' + currentTab).toggleClass('active');
     $('#content').empty('');
-    $('.top20-container').toggleClass('hidden');
+    $('.top20-container').addClass('hidden');
+
+    $('.navBar').on('click', '.genre', function(event) {
+
+      var previousTab = currentTab;
+      currentTab = $(this).attr('id');
+
+      $('#' + previousTab).removeClass('active');
+      $('#' + currentTab).addClass('active');
+
+    });
 
 });
+
+// click on header to go back to main page
+
+$('header').click( function(event) {
+
+    $('.genre').removeClass('active');
+    $('.top20-container').removeClass('hidden');
+    $('.movie-container').addClass('hidden');
+
+});
+
+
+// click on arrow down to expand movie details
 
 $('#container').on('click', 'p.expand-details', function(event) {
 
