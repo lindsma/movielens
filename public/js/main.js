54d37772c5da227cc36fa9031284e4120f779591
Zachary Schreiber <zachgives@gmail.com>
2016-10-09 16:12:48 -0400

    search bar function added

--- public/js/main.js
+++ public/js/main.js
@@ -1,69 +1,51 @@
-
 // On first keypress, ajax search request is made.
 
 $('#userInput').on("keyup", function(event) {
-  if ($('#userInput').val().length > 2) {
- event.preventDefault();
- var searchString = $('#userInput').val();
-movieSearch(searchString);
-}
+    if ($('#userInput').val().length > 2) {
+        event.preventDefault();
+        var searchString = $('#userInput').val();
+        movieSearch(searchString);
+    }
 });
 
 
 //After first keypress, this function takes over.
 
- var $rows = $('.movies');
- $('#userInput').keyup(function() {
-     var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+var $rows = $('.movies');
+$('#userInput').keyup(function() {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
 
-     $rows.show().filter(function() {
-         var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-         return !~text.indexOf(val);
-     }).hide();
- });
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+});
 
- //if enter is pressed, the specific movie is searched
+//if enter is pressed, the specific movie is searched
 
- $('#userInput').keypress(function (event) {
-   var searchString = $('#userInput').val();
-   if (event.which == 13) {
-     movieSearch(searchString);
-     return false;
-   }
- });
+$('#userInput').keypress(function(event) {
+    var searchString = $('#userInput').val();
+    if (event.which == 13) {
+        movieSearch(searchString);
+        return false;
+    }
+});
 
 
-//Test Ajax for Fitch. Right now just for Horror.
-
-   var searchbar = $("#userInput").val("");
+//search bar requests
+function movieSearch(searchString) {
+    var searchbar = $("#userInput").val("");
     $.ajax({
-      "method": "GET",
-       "url": "api/genre/horror",
-       "data":{},
-       "datatype": "json",
-       "success": function(data) {
-         for (var index = 0; index < data.length; index++) {
-        populateMovies(data[index]);
-   }
-       }
-
-      });
-
-      //NavBar gernre reguests
-
-      function movieQuery(response) {
-        $.ajax({
-          "method": "GET",
-           "url": "../api/genre/" + response,
-           "data":{},
-           "datatype": "json",
-           "success": function(data) {
-             for (var index = 0; index < data.length; index++) {
-            populateMovies(data[index]);
-          }
-      }
+        "method": "GET",
+        "url": "/api/movies",
+        "data": {},
+        "datatype": "json",
+        "success": function(data) {
+            for (var index = 0; index < data.length; index++) {
+                populateMovies(data[index]);
+            }
+        }
     });
-    //var junkTitle = "jaws (1975)";
     var apiKey =  'aecec41c5b24a3cdd29ce5c1491c5040';
     var titlePoster = this.data[index].title.substring(0, this.data[index].title.indexOf('('));
      console.log(titlePoster);
@@ -79,42 +61,73 @@ movieSearch(searchString);
         populateMovies(response.results[0].poster_path);
      });
     console.log(poster);
-    }
+}
+//NavBar gernre reguests
+
+function movieQuery(response) {
+    $.ajax({
+        "method": "GET",
+        "url": "../api/genre/" + response,
+        "data": {},
+        "datatype": "json",
+        "success": function(data) {
+            for (var index = 0; index < data.length; index++) {
+                populateMovies(data[index]);
+            }
+        }
+    });
+    //var junkTitle = "jaws (1975)";
+    var apiKey = 'aecec41c5b24a3cdd29ce5c1491c5040';
+    var titlePoster = this.data[index].title.substring(0, this.data[index].title.indexOf('('));
+    console.log(titlePoster);
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.themoviedb.org/3/search/movie?query=" + encodeURIComponent(titlePoster) + "&api_key=" + apiKey,
+        "method": "GET",
+        "processData": false,
+        "data": "{}"
+    };
+    $.ajax(settings).done(function(response) {
+        populateMovies(response.results[0].poster_path);
+    });
+    console.log(poster);
+}
 
 //this.data[index].title
 
 
- // rate movie
+// rate movie
 
- function rateMovie(movieId, movieRating) {
-   if (movieRating === 'delete') {
-     deleteRating(movieId);
-   } else {
-  $.ajax({
-    "method": "POST",
-     "url": "/genre/horror",
-     "data":{},
-     "datatype": "json",
-     "success": function(data) {
-       dataContainer = data;
-     }
-   });
+function rateMovie(movieId, movieRating) {
+    if (movieRating === 'delete') {
+        deleteRating(movieId);
+    } else {
+        $.ajax({
+            "method": "POST",
+            "url": "/genre/horror",
+            "data": {},
+            "datatype": "json",
+            "success": function(data) {
+                dataContainer = data;
+            }
+        });
     }
-  }
+}
 
-  // delete movie
+// delete movie
 
-  function deleteRating(movieId) {
+function deleteRating(movieId) {
     $.ajax({
-      "method": "DELETE",
-       "url": "/genre/horror",
-       "data":{},
-       "datatype": "json",
-       "success": function(data) {
-         dataContainer = data;
-       }
-     });
-  }
+        "method": "DELETE",
+        "url": "/genre/horror",
+        "data": {},
+        "datatype": "json",
+        "success": function(data) {
+            dataContainer = data;
+        }
+    });
+}
 
 
 
@@ -161,7 +174,7 @@ console.log(poster);
 //     });
 // });
 
-$('#action').click( function(event) {
+$('#action').click(function(event) {
     $(this).toggleClass('active');
 
     movieQuery("action");
@@ -210,7 +223,7 @@ function populateMovies(movieObject) {
     var context = {
         avgRating: "8.5",
         // moviePoster: poster,
-        moviePoster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg" ,
+        moviePoster: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg",
         movieTitle: movieObject.title,
         overview: movieObject.url
     };
@@ -250,9 +263,9 @@ function populateErrors() {
 // error handlers
 
 function handleError(errorObject, textStatus, error) {
-        console.log(errorObject, textStatus, error);
-        populateErrors();
-    }
+    console.log(errorObject, textStatus, error);
+    populateErrors();
+}
 
 // handleError();
 // populateMovies();
